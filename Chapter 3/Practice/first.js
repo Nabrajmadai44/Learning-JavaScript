57d546d252aef6c44ef83b374ad4226c6802d8bf
nabrajmadai44 <nabraj0444@gmail.com>
2024-10-18 23:48:14 +0545

    Loops & Strings

--- Chapter 3/Practice/first.js
+++ Chapter 3/Practice/first.js
@@ -26,8 +26,8 @@ for (let num = 0; num <= 100; num++) {
 
 
 /* Qs4. Create a game where you start with any random game number.
-ASk the user to keep guessing the game number number untill the user
-until the use enters correct value */
+ASk the user to keep guessing the game number number untill the user until the use enters correct value */
+
 
 let gameNum = 44;
 let userNum = prompt("Guess the game number : ");
@@ -36,3 +36,15 @@ while (userNum != gameNum) {
     userNum = prompt("You entered wrong number, Guess again : ");
 }
 console.log("Congratulation, you entered the right number.");
+
+
+
+
+/* Qs5. Prompt the user to enter their full name. Generate a username for them based on the input. 
+Start username with @, followed by their full name and ending with the fullname length. */
+
+
+let fullName = prompt("Enter your fullname without Spaces");
+
+let userName = "@" + fullName + fullName.length;
+console.log(userName);
