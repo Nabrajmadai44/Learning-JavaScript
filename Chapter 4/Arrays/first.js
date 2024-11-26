d4638c1016cd840bed341f733e89b742eb37b843
nabrajmadai44 <nabraj0444@gmail.com>
2024-11-26 22:06:39 +0545

    arrays

---
+++ Chapter 4/Arrays/first.js
@@ -0,0 +1,17 @@
+// Arrays : collections of items
+let marks = [87, 54, 78, 89, 99];
+console.log(marks);
+console.log(marks.length); //property
+
+
+let heroes = ["ironman", "shaktiman", "Balveer", "spiderman"];
+console.log(heroes);
+
+
+//Array Indices
+
+
+
+
+// Looping over an Array
+// Print all elements of an array
